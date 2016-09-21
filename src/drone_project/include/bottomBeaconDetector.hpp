8c33f08e67ecfb4abb7611165147c8ba43887977
Saffat S Akanda <jadenandyubel@hotmail.com>
2016-09-21 22:51:34 +1000

    Core logic for detection added, small bugs with compilation of messages. Ranges for blacks and blues must still be tested as well as threshold values. Custom message type must still be added for use, currently using 2 types. WTF IS A MULTIARRAY AND HOW DO I USE IT

--- src/drone_project/include/bottomBeaconDetector.hpp
+++ src/drone_project/include/bottomBeaconDetector.hpp
@@ -14,14 +14,21 @@
 #include <sensor_msgs/Image.h>
 #include "opencv2/imgproc/imgproc.hpp"
 #include "opencv2/highgui/highgui.hpp"
+#include <image_transport/image_transport.h>
+#include <cv_bridge/cv_bridge.h>
 #include <sstream>
 
 namespace drone {
 
 class BottomBeaconDetector {
 private:
-
+    ros::NodeHandle nh;
+    image_transport::ImageTransport it_;            // Made for networking with images, see wiki.ros.org/image_transport
     ros::Subscriber bottomCameraSub;
+    ros::Publisher anglePub;
+    ros::Publisher locationPub;
+    
+    void analyseImage(cv_bridge::CvImagePtr cv_ptr);
 
 public:
     BottomBeaconDetector();
--- src/drone_project/src/bottomBeaconDetector.cpp
+++ src/drone_project/src/bottomBeaconDetector.cpp
@@ -8,31 +8,93 @@
 #include <ros/ros.h>
 
 #include <bottomBeaconDetector.hpp>
-//#include <std_msgs/String.h>
+#include <std_msgs/Float32.h>
+#include <std_msgs/MultiArrayLayout.h>
+#include <std_msgs/String.h>
 
-#define BASE_FRAME  "base_link"
 
 namespace drone {
+using namespace cv;
 
-BottomBeaconDetector::BottomBeaconDetector(){}
+BottomBeaconDetector::BottomBeaconDetector() : it_(nh) {}
 
 void BottomBeaconDetector::configure() {}
 
 void BottomBeaconDetector::startup() {
-    ros::NodeHandle nh;
+    
     bottomCameraSub = nh.subscribe<sensor_msgs::Image>(
-        "ardrone/bottom_raw",
+        "/ardrone/bottom_raw",
         1,
         &BottomBeaconDetector::callbackControl,
          this
     );
+    
+    // Make custom message type later
+    anglePub = nh.advertise<std_msgs::Float32>("/ardrone/beaconAngleRelative", 1);
+    locationPub = nh.advertise<std_msgs::Int32MultiArrayLayout>("/ardrone/beaconLocation", 1);
+    
+    cv::namedWindow(OPENCV_WINDOW);
 }
 
-void BottomBeaconDetector::shutdown(){}
+void BottomBeaconDetector::shutdown() {
+     cv::destroyWindow(OPENCV_WINDOW);
+}
 
 void BottomBeaconDetector::callbackControl(const sensor_msgs::ImageConstPtr& frame) {
     ROS_INFO("BottomBeaconDetector::callbackControl - Image received");
+    cv_bridge::CvImagePtr cv_ptr;
+    
+    try {
+        cv_ptr = cv_bridge::toCvCopy(frame, sensor_msgs::image_encodings::BGR8);
+    }
+    catch (cv_bridge::Exception& e) {
+        ROS_ERROR("cv_bridge exception: %s", e.what());
+        return;
+    }
+    
+    analysePicture(cv_ptr);
+    
+    cv::imshow(OPENCV_WINDOW, cv_ptr->image);
+    image_pub_.publish(cv_ptr->toImageMsg());
 }
 
+void BottomBeaconDetector::analyseImage(cv_bridge::CvImagePtr cv_ptr) {
+    cv::Mat bgr_image = cv_ptr->image;
+    cv::medianBlur(bgr_image, bgr_image, 3);
 
-} // namespace comp3431
+	// Convert input image to HSV
+	cv::Mat hsv_image;
+	cv::cvtColor(bgr_image, hsv_image, cv::COLOR_BGR2HSV);
+	
+	// Have to find better values
+	cv::Mat blackFilterRange;
+	cv::Mat blueFilterRange;
+	cv::inRange(hsv_image, cv::Scalar(0, 0, 0, 0), cv::Scalar(180, 255, 30, 0), blackFilterRange);
+	cv::inRange(hsv_image, cv::Scalar(120, 255, 255), cv::Scalar(120, 255, 255), blueFilterRange);
+	
+	// Find appropriate threshold in testing
+	if (cv::countNonZero(blackFilterRange) < 200) {
+        return;
+    }
+    
+    ROS_INFO("Beacon Detected");
+    
+    vector<Vec2f> lines;
+    vector<Vec3f> circles;
+    
+    HoughLines(blackFilterRange, lines, 1, CV_PI/180, 100, 0, 0);
+    HoughCircles(blueFilterRange, circles, CV_HOUGH_GRADIENT, 1, blueFilterRange.rows/8, 200, 100, 0, 0);
+    
+    anglePub.publish(lines[0][1]);
+    locationPub.publish([circles[0][0], circles[0][1]]);
+    
+}
+
+} // namespace drone
+
+
+
+
+
+
+
