8c3d6e13e31ac21292cb0aabc3d6840b93485d9b
Ben Faul <benjaminfaul@bigpond.com>
2016-09-26 16:34:01 +1000

    detect whole beacon pretty consistently. lots of commented code for showing image in different ways. Useful for exploring different colour channels

--- src/drone_project/src/bottomBeaconDetector.cpp
+++ src/drone_project/src/bottomBeaconDetector.cpp
@@ -47,22 +47,60 @@ void BottomBeaconDetector::callbackControl(const sensor_msgs::ImageConstPtr& fra
         ROS_ERROR("cv_bridge exception: %s", e.what());
         return;
     }
-    
+    /*
+    //split image to colour channels
+    std::vector<cv::Mat> split_img;
+    cv::split(img_ptr->image, split_img);
+    cv::Mat empty_img = cv::Mat::zeros(img_ptr->image.rows, img_ptr->image.cols, CV_8UC1);
+    cv::Mat blue(img_ptr->image.rows, img_ptr->image.cols, CV_8UC3);
+    cv::Mat green(img_ptr->image.rows, img_ptr->image.cols, CV_8UC3);
+    cv::Mat red(img_ptr->image.rows, img_ptr->image.cols, CV_8UC3);
+    //blue
+    cv::Mat in1[] = {split_img[0], empty_img, empty_img};
+    int from_to1[] = {0,0,1,1,2,2};
+    cv::mixChannels(in1, 3, &blue, 1, from_to1, 3);
+    //green
+    cv::Mat in2[] = {empty_img, split_img[1], empty_img};
+    int from_to2[] = {0,0,1,1,2,2};
+    cv::mixChannels(in2, 3, &green, 1, from_to2, 3);
+    //red
+    cv::Mat in3[] = {empty_img, empty_img, split_img[2]};
+    int from_to3[] = {0,0,1,1,2,2};
+    cv::mixChannels(in3, 3, &red, 1, from_to3, 3);   
+
+    cv::namedWindow("blue", CV_WINDOW_AUTOSIZE);
+    cv::imshow("blue", blue); 
+    cv::namedWindow("green", CV_WINDOW_AUTOSIZE);
+    cv::imshow("green", green);
+    cv::namedWindow("red", CV_WINDOW_AUTOSIZE);
+    cv::imshow("red", red);
+    */
     cv::Mat grey_img;
     cv::cvtColor(img_ptr->image, grey_img, CV_BGR2GRAY);
-
+    /*
+    cv::Mat bw_img;
+    bw_img = grey_img > 180;
+    cv::namedWindow("Image Black-White", CV_WINDOW_AUTOSIZE);
+    cv::imshow("Image Black-White", bw_img);
+    cv::waitKey(3);
+    */
+    cv::GaussianBlur(grey_img, grey_img, cv::Size(9,9), 2, 2);
     // This code displays the greyscale image - used for testing.
     //cv::namedWindow("Image Greyscale", CV_WINDOW_AUTOSIZE);
     //cv::imshow("Image Greyscale", grey_img);
     //cv::waitKey(3);
 
     std::vector<cv::Vec3f> circles;
-    cv::HoughCircles(grey_img, circles, CV_HOUGH_GRADIENT, 2, 5, 200, 100, 0, 1000);
+    // The last parameter is the max radius to detect. The whole black beacon is 70-80 pixels
+    // reducing this to 50 should detect only small circles, but it doesnt. Maybe other 
+    // parameters can be tweaked to detect the small circles only.
+    cv::HoughCircles(grey_img, circles, CV_HOUGH_GRADIENT, 2, 20, 200, 100, 0, 100);
     ROS_INFO("finished detection. Found %d circles", int(circles.size())); 
     //draw circles detected
     for(int i = 0; i < circles.size(); i++){
         cv::Point center(cvRound(circles[i][0]), cvRound(circles[i][1]));
         int radius = cvRound(circles[i][2]);
+        //ROS_INFO("circle has radius = %d\n", radius);
         cv::circle(img_ptr->image, center, 3, cv::Scalar(0,255,0), -1, 8, 0);
         cv::circle(img_ptr->image, center, radius, cv::Scalar(0,0,255), 3, 8, 0);
     }
