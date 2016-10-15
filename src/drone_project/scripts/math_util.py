4187b140c331da62fbbdc137dfaf78fc9580135e
Ben Soh <neb_hos@Hotmail.com>
2016-10-15 23:10:42 +1100

    created a pattern detection class

---
+++ src/drone_project/scripts/math_util.py
@@ -0,0 +1,21 @@
+'''
+Math utility methods
+Ben Soh
+'''
+import math
+
+
+def rotate_point(point, rad):
+    x, y = point
+    new_x = x * math.cos(rad) - y * math.sin(rad)
+    new_y = y * math.cos(rad) + x * math.sin(rad)
+    return (int(new_x), int(new_y))
+
+def line_angle(a, b):
+    x1, y1 = a
+    x2, y2 = b
+    if x1 > x2:
+        x1, x2 = x2, x1
+        y1, y2 = y2, y1
+    gradient = float(y2 - y1) / (x2 - x1)
+    return math.atan(gradient)
---
+++ src/drone_project/scripts/pattern.py
@@ -0,0 +1,12 @@
+'''
+Pattern class
+Ben Soh
+'''
+
+class Pattern(object):
+    def __init__(self, x, y, ID, rotation, circles=[]):
+        self.x = x
+        self.y = y
+        self.ID = ID
+        self.rotation = rotation
+        self.circles = circles
