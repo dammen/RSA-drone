a7b970466afd68b5b4a1277f12ec5c35ad218a4f
RSA <jodammen93@gmail.com>
2016-10-17 20:21:00 -0700

    nothing new

--- build/catkin_generated/setup_cached.sh
+++ build/catkin_generated/setup_cached.sh
@@ -8,6 +8,10 @@
 
 # modified environment variables
 export CMAKE_PREFIX_PATH="/home/rsa/rsa_drone/devel:$CMAKE_PREFIX_PATH"
+export CPATH="/home/rsa/rsa_drone/devel/include:$CPATH"
+export LD_LIBRARY_PATH="/home/rsa/rsa_drone/devel/lib:$LD_LIBRARY_PATH"
+export PKG_CONFIG_PATH="/home/rsa/rsa_drone/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
 export PWD="/home/rsa/rsa_drone/build"
+export PYTHONPATH="/home/rsa/rsa_drone/devel/lib/python2.7/dist-packages:$PYTHONPATH"
 export ROSLISP_PACKAGE_DIRECTORIES="/home/rsa/rsa_drone/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
 export ROS_PACKAGE_PATH="/home/rsa/rsa_drone/src:$ROS_PACKAGE_PATH"
