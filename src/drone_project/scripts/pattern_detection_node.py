6c9186548d498eaa2e8cbf78104eb683fbb4c74d
Ben Soh <neb_hos@Hotmail.com>
2016-10-19 16:41:32 +1100

    created node

---
+++ src/drone_project/scripts/pattern_detection_node.py
@@ -0,0 +1,31 @@
+import sherlock_holmes as holmes
+import rospy
+from sensor_msgs.msg import Image
+from std_msgs.msg import Int8
+from geometry_msgs.msg import Point
+import pattern as p
+
+NODE_NAME = 'pattern_detector'
+
+class PatternDetector(object):
+    def __init__(self):
+        self.pattern_id_publisher = rospy.Publisher('pattern_id', Int8, queue_size=10)
+        self.pattern_pos_publisher = rospy.Publisher('pattern_pos', Point, queue_size=10)
+        self.pattern_rot_publisher = rospy.Publisher('pattern_rot', Int8, queue_size=10)
+        self.sherlock = holmes.SherlockHolmes()
+
+    def image_callback(self, data):
+        self.sherlock.image = data.data
+        pattern = self.sherlock.detect_pattern()
+        self.pattern_id_publisher.publish(data=pattern.ID)
+        self.pattern_pos_publisher.publish(x=pattern.x, y=pattern.y)
+        self.pattern_rot_publisher.publish(data=int(pattern.rotation))
+
+def main():
+    rospy.init_node(NODE_NAME, anonymous=True)
+    thing = PatternDetector()
+    rospy.Subscriber('/ardrone/bottom/image_raw', Image, thing.image_callback)
+    rospy.spin()
+
+if __name__ == '__main__':
+    main()
