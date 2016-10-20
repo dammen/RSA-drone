0f2591f843d488a3c3b9dd00439e8c50290f6131
Saffat S Akanda <jadenandyubel@hotmail.com>
2016-10-20 15:06:36 +1100

    moved scripts to src folder

---
+++ src/drone_project/src/pattern_circle.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+'''
+Circle class for patterns
+Ben Soh
+'''
+import math
+
+
+class PatternCircle(object):
+    def __init__(self, x, y, c):
+        self.colour = c
+        self.x = x
+        self.y = y
+
+    def distance(self, other):
+        return math.sqrt((self.x - other.x)**2 + (self.y - other.y)**2)
---
+++ src/drone_project/src/pattern_detection.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+import sherlock_holmes as holmes
+import cv2
+import argparse
+from image_transport import subscriber
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--image', help='look at an image')
+parser.add_argument('--video', help='look at a video')
+args = parser.parse_args()
+
+file = cv2.VideoCapture(args.video) if args.video else cv2.imread(args.image)
+
+# frame counter
+counter = 0
+
+# pattern detector (detective)
+sherlock = holmes.SherlockHolmes(draw=True)
+
+# every x frames, try and detect a pattern
+INTERVAL = 10
+
+while True:
+    # capture a frame from the video
+#    if args.video:
+ #       ret, sherlock.image = file.read()
+  #      if not ret:
+   #         print('Video complete')
+    #        break
+    #elif args.image:
+        sherlock.image = file
+    
+    counter += 1
+    if counter % INTERVAL == 0 or args.image:
+        pattern = sherlock.detect_pattern()
+        if pattern:
+            print('Pattern {0} detected at ({1}, {2}) with a rotation of {3} degrees'.format(pattern.ID, pattern.x, pattern.y, pattern.rotation))
+        else:
+            print('Failed to detect a pattern')
+
+    # display the frame
+    cv2.imshow('Drone', sherlock.image)
+
+    # close if we press escape
+    wait_length = 1 if args.video else 0
+    k = cv2.waitKey(wait_length) & 0xFF
+    if k is 27 or args.image: break
+
+# end
+if args.video:
+    file.release()
+cv2.destroyAllWindows()
