448ea7b44adc80bf297df3f29396444aaf2f1ffc
Ben Soh <neb_hos@Hotmail.com>
2016-10-22 16:03:15 +1100

    converted ros image to cv image

--- src/drone_project/src/pattern_detection_node.py
+++ src/drone_project/src/pattern_detection_node.py
@@ -5,30 +5,36 @@ from sensor_msgs.msg import Image
 from std_msgs.msg import Int8
 from geometry_msgs.msg import Point
 import pattern as p
+from cv_bridge import CvBridge, CvBridgeError
 from drone.msg import beaconGeometry
 
 NODE_NAME = 'pattern_detector'
 
 class PatternDetector(object):
     def __init__(self):
-    	self.patternPublisher = rospy.Publisher("/ardrone/beaconGeometry", beaconGeometry)
+        self.image_subscriber = rospy.Subscriber('/ardrone/bottom/image_raw', Image, self.image_callback)
+        self.patternPublisher = rospy.Publisher("/ardrone/beaconGeometry", beaconGeometry)
         self.pattern_id_publisher = rospy.Publisher('pattern_id', Int8, queue_size=10)
         self.pattern_pos_publisher = rospy.Publisher('pattern_pos', Point, queue_size=10)
         self.pattern_rot_publisher = rospy.Publisher('pattern_rot', Int8, queue_size=10)
+        self.bridge = CvBridge()
         self.sherlock = holmes.SherlockHolmes()
 
     def image_callback(self, data):
-        self.sherlock.image = data.data
+        try:
+            cv_image = self.bridge.imgmsg_to_cv(data, "bgr8")
+        except CvBridgeError as e:
+            print(e)
+        self.sherlock.image = cv_image
         pattern = self.sherlock.detect_pattern()
-        self.pattenPublisher.publish(canSeeBeacon=true, positionX=pattern.x, positionY=pattern.y, angle=pattern.rotation)
+        self.pattenPublisher.publish(canSeeBeacon=True, positionX=pattern.x, positionY=pattern.y, angle=pattern.rotation)
         self.pattern_id_publisher.publish(data=pattern.ID)
         self.pattern_pos_publisher.publish(x=pattern.x, y=pattern.y)
         self.pattern_rot_publisher.publish(data=int(pattern.rotation))
 
 def main():
     rospy.init_node(NODE_NAME, anonymous=True)
-    thing = PatternDetector()
-    rospy.Subscriber('/ardrone/bottom/image_raw', Image, thing.image_callback)
+    pattern_detector = PatternDetector()
     rospy.spin()
 
 if __name__ == '__main__':
